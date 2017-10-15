36aea93af7ce9acdf1c2007876a8a0b6e410179b
Mircy <mircea.gancea@gmail.com>
2017-10-15 01:15:14 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 //Setting information that will be used through the code
-$config['db_host'] = 'mysqlsrv.dcs.bbk.ac.uk';
-$config['db_user'] = 'mgance01';
-$config['db_pass'] = 'bbkmysql';
-$config['db_name'] = 'mgance01db';
+$config['db_host'] = '';
+$config['db_user'] = '';
+$config['db_pass'] = '';
+$config['db_name'] = '';
 $config['language'] = 'en';
 $config['css'] = 'css/w1fma.css';
 ?>
