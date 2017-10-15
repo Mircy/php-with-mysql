5b5c4b599555a5b7ecf3f6ffd1de086f99230b19
Mircy <mircea.gancea@gmail.com>
2017-10-15 01:08:48 +0100

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Setting information that will be used through the code
-$config['db_host'] = '';
-$config['db_user'] = '';
-$config['db_pass'] = '';
-$config['db_name'] = '';
-$config['language'] = 'en';
-$config['css'] = 'css/w1fma.css';
-?>
